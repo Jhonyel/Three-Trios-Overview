e798c35238a524424cd3d5d4159237d6460afef2
Jhonyel <galvis.j@northeastern.edu>
2024-11-13 15:26:17 -0500

    Update TestMaxNumFlipsStrategy.java

--- test/cs3500/three/trios/player/TestMaxNumFlipsStrategy.java
+++ test/cs3500/three/trios/player/TestMaxNumFlipsStrategy.java
@@ -75,6 +75,12 @@ public class TestMaxNumFlipsStrategy {
   }
 
   @Test
+  public void testMaxNumFlipsWithNullModel() {
+    Assert.assertThrows(IllegalArgumentException.class,
+            () -> moveStrategy.getMove(null));
+  }
+
+  @Test
   public void testMaxNumFlipsWithEmptyBoard() {
     List<Card> redHand = new ArrayList<>(handOfFiveWeakestCards);
     List<Card> blueHand = new ArrayList<>(handOfFiveWeakestCards);
